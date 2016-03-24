30dc8fbb217c813ee2f9c28e2fd313a3911150ec
Reece Bedding <reece.bedding@digital.voa.gov.uk>
2016-03-24 14:40:26 +0000

    Added angular app for frontend, and rendered stored vendors from new vendor api

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,10 +2,13 @@ var homeController = require('./homeController.js');
 var authController = require('./authController.js');
 var adminController = require('./adminController.js');
 
+var apiVendorController = require('./api/vendorController.js');
+
 (function(controllers){
     controllers.init = function(app){
         homeController.init(app);
         authController.init(app);
         adminController.init(app);
+        apiVendorController.init(app);
     }
 })(module.exports);
